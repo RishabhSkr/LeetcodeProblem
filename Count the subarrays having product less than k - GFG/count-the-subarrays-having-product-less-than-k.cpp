4a3d853a38596caf718c3d33a5a5d55bd2278583
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2023-07-04 15:15:30 +0530

    Added solution - LeetHub

--- Count the subarrays having product less than k - GFG/count-the-subarrays-having-product-less-than-k.cpp
+++ Count the subarrays having product less than k - GFG/count-the-subarrays-having-product-less-than-k.cpp
@@ -9,18 +9,33 @@ using namespace std;
 
 class Solution{
   public:
+  //Approach 1 T.C : O(n^2)
+//   Approach 2: O(n) O(1)
     int countSubArrayProductLessThanK(const vector<int>& a, int n, long long k) {
            
             int res=0;
-        for(int i = 0 ; i<n; ++i){
-            long p=1;
-            for(int j=i ; j<n;++j){
-                p=p*a[j];
-                if(p<k){
-                    res++;
-                }
-                else break;
+        // for(int i = 0 ; i<n; ++i){
+        //     long p=1;
+        //     for(int j=i ; j<n;++j){
+        //         p=p*a[j];
+        //         if(p<k){
+        //             res++;
+        //         }
+        //         else break;
+        //     }
+        // }
+        int  i=0,j=0;
+        long p =1;
+        while(j<n){
+            p*=a[j];
+            
+            while(i<=j && p>=k){
+                p/=a[i];
+                ++i;
             }
+            if(p<k)
+                res+=j-i+1;
+            j++;
         }
         return res;
     }
