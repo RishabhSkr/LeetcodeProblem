cfbedf7eb32b34c849c0ecb780b11c6a3614e0e6
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2023-07-16 02:16:38 +0530

    Added solution - LeetHub

---
+++ Get Minimum Squares - GFG/get-minimum-squares.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+	public:
+	int MinSquares(int n)
+	{
+	    vector<int> dp(n+1,n);
+	    dp[0]=0;
+	    if(dp[n]!=n)return dp[n];
+	    
+	    for(int i =1;i<=n; ++i){
+	        for(int j =1;j*j<=n;++j){
+	            if(i-j*j>=0)
+	            dp[i]=min(dp[i],1+dp[i-j*j]);
+	        }
+	    }
+	    return dp[n];
+	    
+	}
+};
+
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		Solution ob;
+		int ans = ob.MinSquares(n);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
