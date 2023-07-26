c24c55a695ff58aaaa3b64766e21136d4f08a13b
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2023-07-26 12:25:50 +0530

    Added solution - LeetHub

--- Kth Ancestor in a Tree - GFG/kth-ancestor-in-a-tree.cpp
+++ Kth Ancestor in a Tree - GFG/kth-ancestor-in-a-tree.cpp
@@ -114,24 +114,75 @@ struct Node
 
 // store all ancestors in an array
 
-bool solve(Node * root,int node,vector<int>&ans){
+// bool solve(Node * root,int node,vector<int>&ans){
     
-    if(root == NULL)return false;
+//     if(root == NULL)return false;
     
-    ans.push_back(root->data);
-    if(root->data==node)return true;
-    if(solve(root->left,node,ans))return true;
-    if(solve(root->right,node,ans)) return true;
+//     ans.push_back(root->data);
+//     if(root->data==node)return true;
+//     if(solve(root->left,node,ans))return true;
+//     if(solve(root->right,node,ans)) return true;
     
-    ans.pop_back();
-    return false;
+//     ans.pop_back();
+//     return false;
+// }
+// int kthAncestor(Node *root, int k, int node)
+// {
+//     vector<int>ans;
+//     solve(root,node,ans);
+//     int n  = ans.size();
+//     if(n-1-k<0)return -1;
+//     return ans[n-1-k];
+    
+// }
+
+// your task is to complete this function
+Node* solve(Node* root, int &k, int node) {
+    //base case
+    if(root == NULL)
+        return NULL;
+        
+    if(root->data == node) 
+    {
+        return root;
+    }
+    
+    Node* leftAns = solve(root->left, k, node);
+    Node* rightAns = solve(root->right, k, node);
+
+
+    //wapas
+    if(leftAns != NULL && rightAns == NULL) 
+    {
+        k--;
+        if(k<=0) 
+        {
+            //answer lock
+            k = INT_MAX;
+            return root;
+        }
+        return leftAns;
+    }
+    
+    if(leftAns == NULL && rightAns != NULL) {
+        k--;
+        if(k<=0) 
+        {
+            //answer lock
+            k = INT_MAX;
+            return root;
+        }
+        return rightAns;
+    }
+    return NULL;
+    
+
 }
 int kthAncestor(Node *root, int k, int node)
 {
-    vector<int>ans;
-    solve(root,node,ans);
-    int n  = ans.size();
-    if(n-1-k<0)return -1;
-    return ans[n-1-k];
-    
+    Node* ans = solve(root, k, node);
+    if(ans == NULL || ans->data == node)
+        return -1;
+    else
+        return ans->data;
 }
