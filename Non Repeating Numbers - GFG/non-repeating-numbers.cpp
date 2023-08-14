2f60b42e27162410eaa0d041650c2e1bb4863afc
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2023-08-14 10:13:49 +0530

    Added solution - LeetHub

---
+++ Non Repeating Numbers - GFG/non-repeating-numbers.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> singleNumber(vector<int> nums) 
+    {
+        set<int>st;
+        for(auto x : nums){
+            auto it = st.find(x);
+            if(it!=st.end())st.erase(it);
+            else st.insert(x);
+            
+        }
+        vector<int>ans(st.begin(),st.end());
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	vector<int> v(2 * n + 2);
+    	for(int i = 0; i < 2 * n + 2; i++)
+    		cin >> v[i];
+    	Solution ob;
+    	vector<int > ans = ob.singleNumber(v);
+    	for(auto i: ans)
+    		cout << i << " ";
+    	cout << "\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
