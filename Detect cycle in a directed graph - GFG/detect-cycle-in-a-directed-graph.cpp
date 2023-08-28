922366f3abb206e72d21d5d3e1f260cdfb373fcf
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2023-08-28 10:45:39 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -5,75 +5,47 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-//     // Function to detect cycle in a directed graph.
-//   bool dfs(int node, vector<int>adj[], vector<int>& vis, vector<int>& visPath) {
-//   vis[node] = 1;
-//   visPath[node] = 1;
-
-//   for (int neighbor : adj[node]) {
-//     if (!vis[neighbor]) {
-//       if (dfs(neighbor, adj, vis, visPath)) {
-//         return true;
-//       }
-//     } else if (visPath[neighbor]) {
-//       return true;
-//     }
-//   }
-
-//   visPath[node] = 0;
-//   return false;
-// }
-
-// bool isCyclic(int V, vector<int>graph[]) {
-//   vector<int> vis(V, 0);
-//   vector<int> visPath(V, 0);
-
-//   for (int i = 0; i < V; ++i) {
-//     if (!vis[i]) {
-//       if (dfs(i, graph, vis, visPath)) {
-//         return true;
-//       }
-//     }
-//   }
-
-//   return false;
-// }
-
-    vector<int>vis;
-    vector<int>visPath;
-    
-    bool dfs(int node,vector<int>adj[]){
-        vis[node]=1;
-        visPath[node]=1;
-        
-        for(auto it : adj[node]){
-            
-            if(!vis[it]){
-                // when the node is not visited
-                if(dfs(it,adj)) return true;
-            }
-                // if the node has been previously visited
-			    // but it has to be visited on the same path
-            else if(visPath[it])return true;
-        }
-        visPath[node]=0;
-        return false;
-        
-    }
-    bool isCyclic(int V, vector<int> graph[]) {
-        vis.assign(V,0);
-        visPath.assign(V,0);
-        
-        for(int i=0 ;i<V;++i){
-            
-            if(!vis[i]){
-                
-                if(dfs(i,graph)==true)return true;
-            }
-        }
-        return false;        
-    } 
-
+    // Function to detect cycle in a directed graph.
+    bool isCyclic(int V, vector<int> adj[]) {
+        // code here:  Kahn Algorithm
+	   
+	    // step 1: calulate indegree of nodes
+	    int indegree[V]={0};
+	    
+	    for(int u = 0 ;u<V ; ++u){
+	        for(auto v : adj[u]){
+	            
+	                indegree[v]++;
+	            
+	        }
+	    }
+	    
+	   // step 2: push indegree == 0 in queue 
+	    queue<int>q;
+	    for(int i = 0 ;i<V;i++){
+	        if(indegree[i]==0){
+	            q.push(i);
+	        }
+	    }
+	    
+	   // step 3: pop out all the element who is hae indegree ==0 one by one 
+	   // and make them indepenedent by removing edges and repeate step2;
+	   int cnt =0;
+	   while(!q.empty()){
+	               
+	       int node = q.front();
+	       q.pop();
+	       cnt++;
+	        for(auto it : adj[node]){
+	           indegree[it]--;
+	           if(indegree[it]==0)q.push(it);
+	        }
+	                
+	   }
+	    if(cnt<V)return true;
+	    return false;
+	    
+	}
 };
 
 //{ Driver Code Starts.
