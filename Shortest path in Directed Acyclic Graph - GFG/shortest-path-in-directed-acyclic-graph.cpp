aab28bee916c34ab81f77f882a536d0bdcbd36dc
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2023-08-29 17:00:24 +0530

    Added solution - LeetHub

--- Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
@@ -8,50 +8,39 @@ using namespace std;
 // User function Template for C++
 class Solution {
   public:
- void dfs(int node, vector<bool>&vis, vector<pair<int,int>>adj[],vector<int>&topo){
-    vis[node]=true;
-    
-    for(auto it:adj[node]){
-        if(!vis[it.first]){
-            dfs(it.first,vis,adj,topo);
-        }
-    }
-    
-    topo.push_back(node);
-}
      vector<int> shortestPath(int N,int M, vector<vector<int>>& edges){
-         vector<pair<int,int>> adj[N];
-        for(auto x:edges){
-            adj[x[0]].push_back({x[1],x[2]});
+        // code here
+        vector<pair<int,int>>adj[N];
+        vector<int>dist(N,INT_MAX);
+        for(auto it : edges){
+            int u =it[0];
+            int v =it[1];
+            int wt = it[2];
+            adj[u].push_back({v,wt});
         }
-        vector<bool> vis(N,false);
-        vector<int> topo;
-        for(int i=0;i<N;i++){
-            if(!vis[i]){
-                dfs(i,vis,adj,topo);
-            }
-        }
-          
-        reverse(topo.begin(),topo.end()); //why becoze adj edges are pushing first (i.ev is pushing first than u)
-            
-        vector<int>dis(N,INT_MAX);
         
-        dis[0]=0;
-        for(int i = 0 ; i<N; ++i){
-            if(dis[topo[i]]!=INT_MAX){
-                for(auto x : adj[topo[i]]){
-                    if(dis[topo[i]] + x.second<dis[x.first]){
-                        dis[x.first]=x.second+dis[topo[i]];
-                    }
+        
+        queue<int>q;
+        q.push(0);
+        dist[0]=0;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            
+            for(auto it : adj[node]){
+                int x = it.first;
+                int dis = it.second;
+                if(dis+dist[node]<dist[it.first]){
+                    dist[it.first]=dis+dist[node];
+                    q.push(it.first);
                 }
             }
         }
-        
-        for(int i= 0 ;i<N;++i){
-            if(dis[i]==INT_MAX)dis[i]=-1;
+        for(int i = 0 ; i<N ;++i){
+            if(dist[i]==INT_MAX)dist[i]=-1;
         }
-            return dis;
-            
+        return dist;
+        
     }
 };
 
