7e11744855d0ed296f5f1a769b16c1b0cc1efbf5
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2023-09-05 22:47:02 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,59 +3,75 @@
 using namespace std;
 
 // } Driver Code Ends
+
+class DisjointSet {
+    vector<int> parent, size;
+public:
+    DisjointSet(int n) {
+       
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-	
-// void printMST(int V, vector<int> parent, vector<vector<int>> adj[]) {
-//     cout << "Edges in Minimum Spanning Tree:" << endl;
-//     for (int i = 1; i < V; i++) {
-//         if (parent[i] != -1 && parent[i] < V && i < V && parent[i] >= 0 && i >= 0) {
-//             cout << "Edge: " << parent[i] << " - " << i << " (Weight: " << adj[i][parent[i]][1] << ")" << endl;
-//         }
-//     }
-    
-// }
-int spanningTree(int V, vector<vector<int>> adj[])
-{
-    vector<int> parent(V, INT_MAX);
-    vector<int> dist(V, INT_MAX);
-    vector<int> vis(V, -1);
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+       vector<pair<int, pair<int, int>>> edges;
+        for (int i = 0; i < V; i++) {
+            for (auto it : adj[i]) {
+                int adjNode = it[0];
+                int wt = it[1];
+                int node = i;
 
-    // wt, {node, parent}
-    priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int, int>>>,
-    greater<pair<int,pair<int, int>>>> pq;
-    dist[0] = 0;
-    parent[0] = -1;
-    pq.push({0, {0, -1}});
-
-    int sum = 0;
-    while (!pq.empty()) {
-        auto current = pq.top();
-
-        int wt = current.first;
-        int node = current.second.first;
-        int pare = current.second.second;
-        pq.pop();
-        if (vis[node] == 1) continue;
-		// add it to the mst
-		vis[node] = 1;
-		sum += wt;
-        for (auto neighbor : adj[node]) {
-            int v = neighbor[0];   // Corrected indexing
-            int cost = neighbor[1]; // Corrected indexing
-
-            if (vis[v] == -1) {
-                dist[v]=cost;
-                parent[v] = pare;
-                pq.push({dist[v], {v, pare}});
+                edges.push_back({wt, {node, adjNode}});
             }
         }
-    }
-    return sum;
-}
+        DisjointSet ds(V);
+        sort(edges.begin(), edges.end());
+        int mstWt = 0;
+        for (auto it : edges) {
+            int wt = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
 
+            if (ds.findUPar(u) != ds.findUPar(v)) {
+                mstWt += wt;
+                ds.unionBySize(u, v);
+            }
+        }
+
+        return mstWt;
+        
+    }
 };
 
 //{ Driver Code Starts.
