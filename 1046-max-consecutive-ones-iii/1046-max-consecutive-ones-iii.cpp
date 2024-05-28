f473c7957148eaf1a89eb1932b226e7696ccc0ba
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-05-28 19:34:07 +0530

    Time: 46 ms (30.17%), Space: 57.9 MB (48.5%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -5,9 +5,19 @@ public:
         int i =0;
         int n =nums.size();
         int maxLen = 0;
-        for(int j = 0;j<n;++j){
+        // This not simple one -> while loop is there but we can eliminate the whlile
+        // logic-> increase window size till k zeroes. if greater than k zeroo in obtained decrease the window size till you get the zeroes<=k . this again extra overhead  can be ubderstand  11111000 k=2
+        // for(int j = 0;j<n;++j){
+        //     if(nums[j]==0)cnt0++;
+        //     while(cnt0>k){
+        //         if(nums[i]==0)cnt0--;
+        //         i++;
+        //     }
+        //     if(cnt0<=k)maxLen = max(j-i+1,maxLen);
+        // }
+         for(int j = 0;j<n;++j){
             if(nums[j]==0)cnt0++;
-            while(cnt0>k){
+            if(cnt0>k){
                 if(nums[i]==0)cnt0--;
                 i++;
             }
