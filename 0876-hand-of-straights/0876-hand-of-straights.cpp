4af95bbe3a6013df6da192e2f744a5946c4f1880
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-06-07 02:09:16 +0530

    Time: 61 ms (53.2%), Space: 32.2 MB (19.28%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.cpp
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
         if(hand.size()%groupSize)return false;
-         priority_queue<int,vector<int>,greater<int>>pq;
+        priority_queue<int>pq; // using max heap as min-heap
         unordered_map<int,int>mp;
-        for(auto x: hand)mp[x]++,pq.push(x);
+        for(auto x: hand)mp[x]++,pq.push(-x);
+       
         while(!pq.empty()){
-           int ele =pq.top();
+           int ele =-pq.top();
            pq.pop();
            if(mp[ele]==0)continue;
             int remain = groupSize-1;
