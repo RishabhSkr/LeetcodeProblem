00a09dbcd297b5d57fbe590df054f2bfdc577928
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-06-08 22:30:19 +0530

    Time: 0 ms (100%), Space: 7.9 MB (50%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string ans ="";
+        for(int i =0;i<s.size();++i){
+            if(isdigit(s[i]))ans.pop_back();
+            else ans+=s[i];
+        }
+        return ans;
+    }
+};
