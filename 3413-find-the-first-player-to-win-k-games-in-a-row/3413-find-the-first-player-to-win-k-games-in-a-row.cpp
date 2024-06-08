a3ddcdef6419966acd825a778baebb0724ebaf5a
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-06-09 00:01:22 +0530

    Time: 185 ms (75%), Space: 119.7 MB (37.5%) - LeetHub

--- 3413-find-the-first-player-to-win-k-games-in-a-row/3413-find-the-first-player-to-win-k-games-in-a-row.cpp
+++ 3413-find-the-first-player-to-win-k-games-in-a-row/3413-find-the-first-player-to-win-k-games-in-a-row.cpp
@@ -1,39 +1,16 @@
 class Solution {
 public:
     int findWinningPlayer(vector<int>& skills, int k) { 
-     deque<pair<int, int>> dq; // Store pairs of (skill, index)
-        int maxi = -1,max_idx = -1;
-        for (int i = 0; i < skills.size(); ++i) {
-            dq.push_back({skills[i], i});
+      deque<pair<int,int>>dq;
+        int maxi = skills[0],maxIdx =0,n = skills.size(),cnt=0;
+        for(int i =1;i<n;++i){
             if(maxi<skills[i]){
-                maxi = skills[i];
-                max_idx = i;
+                maxi=skills[i];
+                maxIdx =i;
+                cnt=0;
             }
-    
+            if(++cnt>=k)break;
         }
-        if(k>skills.size())return max_idx;
-        int win_streak = 0;
-        pair<int, int> curr = dq.front();
-        dq.pop_front();
-
-        while (win_streak < k && !dq.empty()) {
-            pair<int, int> p2 = dq.front();
-            dq.pop_front();
-
-            if (curr.first > p2.first) {
-                ++win_streak;
-                dq.push_back(p2);
-            } else {
-                dq.push_back(curr); 
-                curr = p2;
-                win_streak = 1;
-            }
-
-            if (curr.first == maxi) {
-                return curr.second;
-            }
-        }
-
-        return curr.second;
+        return maxIdx;
     }
-}; // 28063
+}; 
