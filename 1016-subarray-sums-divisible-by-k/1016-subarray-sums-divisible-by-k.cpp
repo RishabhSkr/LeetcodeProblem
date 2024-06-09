be50b8b366522f7cc662fa7644411592795776e4
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-06-09 06:44:46 +0530

    Time: 36 ms (74.52%), Space: 35.1 MB (44.61%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
@@ -7,7 +7,7 @@ public:
         for(int i =0;i<n;++i){
             sum+=nums[i];
             int mod= (sum%k+k)%k; // for negative integers it will add modulo with negative sign<k so if we add k to it become positive but wait we adding sum to it may be possible it can greater upto k we again have to take modulo
-            cout<<mod<<endl;
+            // cout<<mod<<endl;
             if(mp.count(mod)>0)cnt+=mp[mod];
             mp[mod]++;
         }
