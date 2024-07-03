3f7dd17eb21e94edcb8c45ad2ec5b51b2c289182
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-07-03 22:40:55 +0530

    Time: 72 ms (39.46%), Space: 38.8 MB (15.52%) - LeetHub

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -6,11 +6,15 @@ public:
         if(n<5)return 0;
         int ans =INT_MAX;
             // 3 moves ka use piche ele ko change krne me laga do.max ele n-4 pr hga
-            ans = min(ans,nums[n-4]-nums[0]);
-            ans = min(ans,nums[n-3]-nums[1]);
-            ans = min(ans,nums[n-2]-nums[2]); // 2 front aur 1 piche
-            ans = min(ans,nums[n-1]-nums[3]); // 3 moves me 0 1 2 ko change 4th elem min hga
-             
+            // ans = min(ans,nums[n-3-1]-nums[0]);
+            // ans = min(ans,nums[n-2-1]-nums[1]);
+            // ans = min(ans,nums[n-1-1]-nums[2]); // 2 front aur 1 piche
+            // ans = min(ans,nums[n-0-1]-nums[3]); // 3 moves me 0 1 2 ko change 4th elem min hga
+             int cnt =3;
+             for(int i =0;i<4;++i){
+                ans = min(ans,nums[n-1-cnt]-nums[i]);
+                cnt--;
+             }
         return ans;
     }
 };
