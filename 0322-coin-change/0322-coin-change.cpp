d5b0ecdd65c0ae9f04063d66f6d1b2e75ebcc7f1
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-07-09 17:28:51 +0530

    Time: 48 ms (30.46%), Space: 46.8 MB (34.14%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
 vector<vector<int>>dp;
-    int solve(int i,vector<int>&arr,int sum){
-        if(i==0){
-            if(sum%arr[0]==0)return sum/arr[0];
-            return 1e9;
-        }
-        if(dp[i][sum]!=-1)return dp[i][sum];
-        int notTake = solve(i-1,arr,sum);
-        int take =1e9;
-        if(arr[i]<=sum){
-            take = 1+solve(i,arr,sum-arr[i]);
-        }
-        return dp[i][sum]=min(take,notTake);
-    }
+
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        dp.resize(n,vector<int>(amount+1,-1));
-        int ans =solve(n-1,coins,amount);
+        dp.resize(n,vector<int>(amount+1,1e9));
+      // understand the base case and your are good to go
+        // method 2 : dp
+        for(int i=0;i<=amount;++i){
+            if(i%coins[0]==0)dp[0][i]=i/coins[0];
+        }
+        for(int i =1;i<n;++i){
+            for(int amt =0;amt<=amount;amt++){
+                int notTake = dp[i-1][amt];
+                int take =1e9;
+                if(coins[i]<=amt){
+                    take = 1+dp[i][amt-coins[i]];
+                }
+                dp[i][amt]=min(take,notTake);   
+            }
+        }
+        int ans = dp[n-1][amount];
         if(ans>=1e9)return -1;
         return ans;
     }
