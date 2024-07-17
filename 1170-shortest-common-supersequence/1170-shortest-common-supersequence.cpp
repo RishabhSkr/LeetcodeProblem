0ef9784e3fd1b37d6c5516bbcf406c342584cbd9
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-07-17 14:59:40 +0530

    Time: 18 ms (82.96%), Space: 25.9 MB (14.63%) - LeetHub

---
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        int n1=str1.size(),n2=str2.size();
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
+        for (int i=0; i<n1; i++){
+            for (int j=0; j<n2; j++){
+                if (str1[i]==str2[j]){
+                    dp[i+1][j+1]=1+dp[i][j];
+                }else{
+                    dp[i+1][j+1]=max(dp[i][j+1], dp[i+1][j]);
+                }
+            }
+        }
+        string ans="";
+        int i = n1,j =n2;
+        while(i>0 && j>0){
+            if(str1[i-1]==str2[j-1]){
+                ans+=str1[i-1];
+                i--;
+                j--;
+            }else{
+                if(dp[i-1][j]>dp[i][j-1]){
+                    ans+=str1[i-1];
+                    i--;
+                }else{
+                    ans+=str2[j-1];
+                    j--;
+                }
+            }
+        }
+        // If there are remaining characters in str1 or str2
+        while (i > 0) {
+            ans += str1[i - 1];
+            i--;
+        }
+        while (j > 0) {
+            ans += str2[j - 1];
+            j--;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+    
+};
