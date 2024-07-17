29967865fe71b99a436189e57e4d49a904422326
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-07-17 22:15:11 +0530

    Time: 3 ms (99.3%), Space: 16.6 MB (78.83%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    int solve(int i ,int j ,string &s,string &t){
-        if(j<0)return 1;
-        if(i<0)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        int take =0,notTake = 0;
-        if(s[i]==t[j]){
-            take = solve(i-1,j-1,s,t)+solve(i-1,j,s,t);
-        }else{
-            notTake = solve(i-1,j,s,t);
-        }   
-        return dp[i][j]=take+notTake;
-    }
+
+  
     int numDistinct(string s, string t) {
         int n = s.size(), m = t.size();
-            memset(dp,-1,sizeof(dp));
-            return solve(n-1,m-1,s,t);
+            double dp[n+1][m+1];
+            memset(dp,0,sizeof(dp));
+         
+            // method 2 : dp top down
+            for(int i = 0;i<=n;++i){
+                dp[i][0]=1;
+            }
+            for(int i = 1;i<=m;++i){ // if from 0 it will rewrite the case of above which leads to wrong ans
+                dp[0][i]=0;
+            }
+            for(int i =1;i<=n;++i){
+                for(int j =1;j<=m;++j){
+                    double take =0,notTake = 0;
+                    if(s[i-1]==t[j-1]){
+                        take = dp[i-1][j-1]+dp[i-1][j];
+                    }else{
+                        notTake = dp[i-1][j];
+                    }   
+                    dp[i][j]=take+notTake;
+                }
+            }
+            return (int)dp[n][m];
     }
 };
