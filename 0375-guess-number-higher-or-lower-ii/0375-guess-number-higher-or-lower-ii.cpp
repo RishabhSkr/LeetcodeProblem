2b4b0d6f729f3c18f5c393261dfad3dff1ff7b80
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-08-10 17:01:16 +0530

    Time: 35 ms (90.87%), Space: 9.7 MB (17.92%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    int solve(int start,int end){
-        if(start>=end)return 0;
-        if(dp[start][end]!=-1)return dp[start][end];
-        int res = INT_MAX;
-        for(int i =start;i<=end;i++){
-            int ans = i+max(solve(start,i-1),solve(i+1,end)); // worst case n!
-            res = min(res,ans);
-        }
-        return dp[start][end]=res;
-    }
+    // int solve(int start,int end){
+    //     if(start>=end)return 0;
+    //     if(dp[start][end]!=-1)return dp[start][end];
+    //     int res = INT_MAX;
+    //     for(int i =start;i<=end;i++){
+    //         int ans = i+max(solve(start,i-1),solve(i+1,end)); // worst case n!
+    //         res = min(res,ans);
+    //     }
+    //     return dp[start][end]=res;
+    // }
     int getMoneyAmount(int n) {
-        dp.resize(n+1,vector<int>(n+1,-1));
-        return solve(1,n);
+        dp.resize(n+2,vector<int>(n+2,0));
+        // return solve(1,n);
+        for(int start =n;start>=1;--start){
+              for(int end =start;end<=n;++end){  
+                if(start==end)continue;
+                int res = INT_MAX;
+                for(int i =start;i<=end;i++){
+                    int ans = i+max(dp[start][i-1],dp[i+1][end]); 
+                    res = min(res,ans);
+                }
+                dp[start][end]=res;
+              }
+        }
+        return dp[1][n];
     }
 };
