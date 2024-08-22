77539560ecb547d26d9376f3aa9aeee1348cc190
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-08-22 17:54:46 +0530

    Time: 0 ms (100%), Space: 7.4 MB (57.13%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -2,10 +2,19 @@ class Solution {
 public:
     int findComplement(int num) {
         int numBits =(int)log2(num)+1; // 5 = 101 = 3  mask==001
-        for(int i =0;i<numBits;++i){
-            num=num^(1<<i); // yaha 1 ko 1-1 bit flip kr rhe..1<<i(0 to n-1th bit) flip
-        }
-        return num;
+        // for(int i =0;i<numBits;++i){
+        //     num=num^(1<<i); // yaha 1 ko 1-1 bit flip kr rhe..1<<i(0 to n-1th bit) flip
+        // }
+        // return num;
+
+        // 2nd Apporach using concept create a bitmask equal to number of bits and take xor with num
+            // how to create a bitmask n bits = num bits
+                // concept = 101 -> 3 bits = 2pow3 = 8-1 = 0111; mask = (pow(2,numBits)-1) or (1<<numBits)-1
+          
+            unsigned int mask = (1U<<numBits)-1;
+            return  num ^ mask; 
+        // 3rd Approach
         
+          
     }
 };
