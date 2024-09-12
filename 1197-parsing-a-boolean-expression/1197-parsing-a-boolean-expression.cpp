04616f29f63a3a12ca85a49663080e823cf6ae8b
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-09-12 21:23:35 +0530

    Time: 18 ms (15.84%), Space: 11.9 MB (28.54%) - LeetHub

---
+++ 1197-parsing-a-boolean-expression/1197-parsing-a-boolean-expression.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+bool parse_or(vector<int>&res){
+    bool ans = res[0];
+    for(auto x : res){
+        ans|=x;
+    }
+    return ans;
+}
+ 
+ bool parse_and(vector<int>&res){
+    bool ans = res[0];
+    for(auto x : res){
+        ans&=x;
+    }
+    return ans;
+}  
+
+ bool parseBoolExpr(string exp) {
+        stack<char>st;
+        bool ans =false;
+        for(auto ch : exp){
+            if(ch==',')continue;
+            if(ch==')'){
+                vector<int>res;
+                while( st.top()!='!' && st.top()!='|' && st.top()!='&'){
+                    char  c = st.top();
+                    st.pop();
+                    if(c=='f')res.push_back(0);
+                    else if(c=='t')res.push_back(1);
+
+                }
+                char c = st.top();
+                st.pop();
+
+                if(c=='|'){
+                    ans = parse_or(res);
+                    if(ans)st.push('t');
+                    else st.push('f');
+                }
+                else if(c=='&'){
+                    ans = parse_and(res);
+                    if(ans)st.push('t');
+                    else st.push('f');
+                }
+                else if(c=='!'){
+                    if(res[0]==0)st.push('t');
+                    else st.push('f');
+                }
+
+            }
+            else st.push(ch);
+
+        }
+        return st.top()=='t';      
+    }
+};
