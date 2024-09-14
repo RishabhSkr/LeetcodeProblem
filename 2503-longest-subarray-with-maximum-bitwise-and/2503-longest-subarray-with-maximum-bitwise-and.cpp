d9b473b476ad10bd43e91b1330bad74d5272a262
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-09-14 11:29:52 +0530

    Time: 99 ms (76.32%), Space: 84.8 MB (77.85%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
@@ -4,26 +4,11 @@ public:
         // find consecutive max element
         int maxi = *max_element(nums.begin(),nums.end());
         int n = nums.size();
-        int cnt = 0,ans=0;
-        bool found=false;
+        int cnt =0,ans =0;
         for(int i =0;i<n;++i){
-            if(maxi==nums[i]){
-                cnt++;
-                found = true;
-                ans = max(cnt,ans);
-                continue;
-            }
-            if(found){
-                if(nums[i-1]==maxi && nums[i]==maxi){
-                    cnt++;
-                    ans = max(ans,cnt);
-                }
-                else{
-                    found = false;
-                    cnt =0;
-                } 
-            }
-            
+           if(nums[i]==maxi){
+            ans =  max(ans,++cnt);
+           }else cnt =0;
         }
         return ans;
 
