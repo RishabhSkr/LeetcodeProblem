71c8edcea57ce34c1b7495fa6438a84bc4e2490c
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-09-19 20:57:07 +0530

    Time: 0 ms (100%), Space: 13.6 MB (61.78%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -1,31 +1,38 @@
 class Solution {
 public:
-    vector<int> solve(string s){
-        vector<int>res;
-        for(int i =0;i<s.size();++i){
-            if(s[i]=='-' || s[i]=='+'||s[i]=='*'){
+    vector<int> solve(const string &s, int i, int j) {
+        if (i > j) return {};
 
-                vector<int>leftRes = solve(s.substr(0,i));
-                vector<int>rightRes = solve(s.substr(i+1));
+        vector<int> res;
+        // Iterate between i and j, not the whole string
+        for (int k = i; k <= j; ++k) {
+            if (s[k] == '-' || s[k] == '+' || s[k] == '*') {
+                vector<int> leftRes = solve(s, i, k - 1);
+                vector<int> rightRes = solve(s, k + 1, j);
 
-                for(int num1 : leftRes){
-                    for(int num2 : rightRes){
-                        if(s[i]=='+'){
-                            res.emplace_back(num1+num2);
-                        }else if(s[i]=='-'){
-                            res.emplace_back(num1-num2);
-                        }else if(s[i]=='*'){
-                            res.emplace_back(num1*num2);
+                // Combine results from left and right expressions
+                for (int num1 : leftRes) {
+                    for (int num2 : rightRes) {
+                        if (s[k] == '+') {
+                            res.emplace_back(num1 + num2);
+                        } else if (s[k] == '-') {
+                            res.emplace_back(num1 - num2);
+                        } else if (s[k] == '*') {
+                            res.emplace_back(num1 * num2);
                         }
                     }
-
                 }
             }
         }
-        if(res.empty())res.emplace_back(stoi(s));
+
+        // If no operator found, it's a number, so convert the substring from i to j
+        if (res.empty()) {
+            res.emplace_back(stoi(s.substr(i, j - i + 1)));
+        }
         return res;
     }
+
     vector<int> diffWaysToCompute(string exp) {
-        return solve(exp);
+        return solve(exp, 0, exp.size() - 1);
     }
 };
