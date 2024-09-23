67c612b3d149fd5c26389b2211550783d13a59db
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-09-24 00:15:00 +0530

    Time: 99 ms (69.26%), Space: 69.5 MB (72.97%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-vector<int>dp;
-int help(const string &s1,vector<string>&dict,int i){
-    if(i==s1.size())return 0;
-    int ans = 1+help(s1,dict,i+1);
-    if(dp[i]!=-1)return dp[i];
-    for(auto &w : dict){
-        if(s1.compare(i,w.size(),w)==0){
-            ans = min(ans,help(s1,dict,i+w.size()));
+    vector<int>dp;
+    int n;
+    int solve(string s,int i,vector<string>&dict){
+        if(i==n)return 0;
+        if(dp[i]!=-1)return dp[i];
+        int ans = 1+solve(s,i+1,dict);
+        for(auto &w : dict){
+            if(s.substr(i,w.size())==w){
+                ans = min(ans,solve(s,i+w.size(),dict));
+            }
         }
+        return dp[i]=ans;
     }
-    return dp[i]=ans;
-}
     int minExtraChar(string s, vector<string>& dictionary) {
-        dp.assign(51,-1);
-     return help(s,dictionary,0);   
+        n = s.size();
+        dp.resize(n,-1);
+        return solve(s,0,dictionary);
     }
-};
+};  
