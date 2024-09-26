06a1af6c878c81d166fc0f2df1b5766317d22b46
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-09-26 14:50:02 +0530

    Time: 77 ms (66.21%), Space: 41.5 MB (99.66%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -1,56 +1,16 @@
-#include <iostream>
-using namespace std;
 
-class MyListNode {
-public:
-    pair<int, int> data;
-    MyListNode *next;
-
-    MyListNode(pair<int, int> data) {
-        this->data = data;
-        next = nullptr;
-    }
-};
-
-class Node {
-    MyListNode *dummy;
-
-public:
-    Node() {
-        // Dummy node for easier management of the linked list
-        dummy = new MyListNode({-1, -1});  // Use dummy with invalid event time
-    }
-
-    bool insert(MyListNode *newNode) {
-        MyListNode *prev = dummy;
-        MyListNode *curr = dummy->next;
-        // check the node positon
-        while(curr!=nullptr && curr->data.first<newNode->data.first){
-            prev = curr;
-            curr = curr->next;
-        }
-        //check if node is overlapping or not [3,8] newNode [9,14]  [15,30]
-        if(prev!=dummy && prev->data.second >newNode->data.first ||
-            curr!=nullptr && curr->data.first<newNode->data.second ){
-                return false;
-        }
-
-        newNode->next = curr;
-        prev->next = newNode;
-        return true;
-    }
-};
 
 class MyCalendar {
-    Node *list;
-
 public:
+    vector<pair<int,int>>bookings;
     MyCalendar() {
-        list = new Node();
     }
 
     bool book(int start, int end) {
-        return list->insert(new MyListNode({start, end}));
+        for(auto &event : bookings){
+            if(max(event.first,start)<min(event.second,end))return false;
+        }
+        bookings.push_back({start,end});
+        return true;
     }
 };
-
