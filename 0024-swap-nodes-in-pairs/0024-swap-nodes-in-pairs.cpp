05c828f3521be87ad79ab4183ec831c0ebd1b4c8
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-09-28 17:19:42 +0530

    Time: 5 ms (32.06%), Space: 10.7 MB (38.46%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -8,19 +8,66 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+/*
+       #---->@---->@---->@---->@---->@---->@
+                   ^     ^
+                   pre   cur
+
+      1. pre->next = cur->next
+                    __________
+                   /          \
+       #---->@---->@     @---->@---->@---->@
+                   ^     ^
+                   pre   cur
+
+      2. pre = pre->next
+                    __________
+                   /          \
+      #---->@---->@      @---->@---->@---->@
+                         ^     ^
+                         cur   pre
+
+      3. cur->next = pre->next
+                    __________
+                   /          \
+       #---->@---->@     @     @---->@---->@
+                          \_________/
+                         ^     ^
+                         cur   pre
+
+      4. pre->next = cur
+                   __________
+                  /          \
+      #---->@---->@     @<----@     @---->@
+                         \_________/
+                        ^     ^
+                        cur   pre
+    
+      5. pre = cur; cur = cur->next
+                   __________
+                  /          \
+      #---->@---->@     @<----@     @---->@
+                         \_________/
+                        ^           ^
+                        pre         cur
+ */
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
         if(head==NULL || head->next==NULL)return head;
-        ListNode * curr = head;
-        ListNode * nxt = head->next;
-        swap(curr->val,nxt->val);
-        while(curr!=NULL && nxt!=NULL){
-            curr = nxt->next;
-            if(curr==NULL)break;
-            nxt = curr->next;
-            if(nxt!=NULL)swap(curr->val,nxt->val);
+        ListNode * dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode * prev =dummy;
+        ListNode * curr = dummy->next;
+        // method 2 without swapping values swap by pointers
+        while(curr!=NULL && curr->next!=NULL){
+            prev->next = curr->next;
+            prev =prev->next;
+            curr->next = prev->next;
+            prev ->next= curr;
+            prev = curr;
+            curr= curr->next;
         }   
-        return head;
+        return dummy->next;
     }
 };
