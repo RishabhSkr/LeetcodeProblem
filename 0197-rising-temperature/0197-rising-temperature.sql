a50b77803c92a6136af37226b12db53748931038
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-10-03 00:53:45 +0530

    Time: 501 ms (30.04%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
-SELECT a.id 
-FROM Weather as a
-JOIN Weather as b
-ON DATEDIFF(a.recordDate,b.recordDate)=1
-WHERE a.temperature>b.temperature
+SELECT a.id as Id
+FROM Weather as a where exists (
+    select b.id 
+    from weather as b
+    where DATEDIFF(a.recordDate,b.recordDate)=1 and a.temperature >b.temperature
+)
