3e942870a38023e6a7a75c3495df692822debb56
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-10-07 15:37:24 +0530

    Time: 21 ms (5.05%), Space: 10.1 MB (93.92%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
     int minLength(string s) {
-        stack<char>st;
-        int cnt  = s.length();
-        for(auto &ch : s){
-            if (!st.empty() && ((st.top() == 'A' && ch == 'B') || (st.top() == 'C' && ch == 'D'))){
-                st.pop();
-                cnt-=2;
-            }else{
-                st.push(ch);
+        // stack<char>st;
+        // int cnt  = s.length();
+        // for(auto &ch : s){
+        //     if (!st.empty() && ((st.top() == 'A' && ch == 'B') || (st.top() == 'C' && ch == 'D'))){
+        //         st.pop();
+        //         cnt-=2;
+        //     }else{
+        //         st.push(ch);
+        //     }
+        // }
+        // brute force 
+        // ab and cd ko delete krte jao O(n2)
+        while(s.find("AB")!=-1 || s.find("CD")!=-1){
+            
+            if(s.find("AB")!=-1){
+                s.erase(s.find("AB"),2);
+            }else if(s.find("CD")!=-1){
+                s.erase(s.find("CD"),2);
             }
         }
-        return cnt;
+        return s.length();
     }
+    
 };
