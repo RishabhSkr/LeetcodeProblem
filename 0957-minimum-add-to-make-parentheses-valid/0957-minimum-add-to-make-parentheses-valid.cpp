55239e05c4dc0fb8bb23cc764d7c49edcd4df2cd
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-10-09 10:44:52 +0530

    Time: 0 ms (100%), Space: 7.8 MB (81.46%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        int count =0,neg =0;
-        int n =s.length();
-        for(int i =0;i<n;++i){
-            if(s[i]=='(')count++;
-            else count--;
-            if(count<0){
-                neg++;
-                count=0;
+        int op = 0;
+        int need = 0;
+        for(auto  x: s){
+            if(x=='(')op++;
+            else{
+                op--;
+                if(op<0){
+                    need++;
+                    op=0;
+                }
             }
-      
         }
-        return abs(count+neg);
+        return op+need;
     }
 };
