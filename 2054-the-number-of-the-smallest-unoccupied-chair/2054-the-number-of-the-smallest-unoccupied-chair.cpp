31c79bb5c7b460032f0035b04fccb4a5f3879e38
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-10-11 22:40:15 +0530

    Time: 138 ms (75.26%), Space: 58.9 MB (81.79%) - LeetHub

--- 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.cpp
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -1,35 +1,6 @@
 class Solution {
 public:
-        // // Plan-> sort times acc to arr 
-        // // pq => {chairNo,arr,end},chairNo -> 0,1,2...infinity
-        // // use  two min heaps-> store one with 
-        // sort(times.start(),times.end(),[](vector<int>&a,vector<int>&b){
-        //     return a[0]<b[0];
-        // });
-        // // two min for searching
-        // priority_queue<pair<int,vector<int>>,vector<pair<int,vector<int>>>,greater<pair<int,vector<int>>>>pq1,pq2;
-        // int chair=0;
-        // for(auto time : times){
-        //     int s = time[0];
-        //     int e = time[1];
-        //     if(!pq1.empty()){
-        //         auto top = pq1.top();
-        //         int chairNo = top.first;
-        //         auto start = top.second[0];
-        //         auto end = top.second[1];
-        //         pq.pop();
-        //         if(end<=s){
-
-        //         }
-        //     }
-        //     pq1.push({chair++,{s,e}});
-        // }
-        // while(!pq1.empty()){
-        //     int chairNo = top.first;
-        //     int arr
-        // }
-        
-    int smallestChair(vector<vector<int>>& times, int tar) {
+       
         // brute force ->O(n^2) S.C->O(n)
         /*
             plan->
@@ -37,13 +8,6 @@ public:
                 -store the dep time on each chair
                 - For each friend in time traverse the chair and look for chair whose deadline is complete
                 - Look for the chair whose dept time is finished assing it and search for target friend
-        */
-        int n=times.size();
-        int tarArr = times[tar][0]; // because time of arr is unique
-
-        sort(times.begin(),times.end());
-        vector<int>chair(n,-1);
-
         for(auto t : times){
             int arr = t[0];
             int dept = t[1];
@@ -58,6 +22,45 @@ public:
                 
             }
         }
+        */
+        // Optimal->O(nLogn) Plan-> sort times acc to arr searching for each frnd chair taking n times how about decrease it in logn and or less.
+        // pq => {chairNo,arr,end},chairNo -> 0,1,2...infinity
+        // use  two min heaps-> store one with 
+    typedef pair<int,int> P;
+    int smallestChair(vector<vector<int>>& times, int tar) {
+        int n=times.size();
+        int tarArr = times[tar][0]; // because time of arr is unique
+
+        sort(times.begin(),times.end());
+
+        // two min for searching
+        priority_queue<P,vector<P>,greater<P>>pq1;
+        priority_queue<int>pq2;
+        int cnt = 0;
+
+        for(auto time : times){
+            int s = time[0];
+            int dept = time[1];
+            
+
+            while(!pq1.empty() && pq1.top().first<=s){
+                auto chairInd = pq1.top().second;
+                pq2.push(-1*chairInd);
+                pq1.pop();
+            }
+            if(pq2.empty()){
+                pq1.push({dept,cnt});
+                if(s==tarArr)return cnt;
+                cnt++;
+            }
+            else{
+                int smallChair = -1*pq2.top();
+                pq2.pop();
+                if(s==tarArr)return smallChair;
+                pq1.push({dept,smallChair});
+            }
+        }
+
         return -1;
     }
 };
