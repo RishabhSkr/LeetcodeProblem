645264cd6f2b3a2b2126085330a6c782bcc0c92b
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-10-15 18:20:54 +0530

    Time: 230 ms (23.64%), Space: 158.8 MB (57.01%) - LeetHub

--- 3604-find-the-number-of-possible-ways-for-an-event/3604-find-the-number-of-possible-ways-for-an-event.cpp
+++ 3604-find-the-number-of-possible-ways-for-an-event/3604-find-the-number-of-possible-ways-for-an-event.cpp
@@ -1,56 +1,21 @@
 class Solution {
 public:
 #define int long long
-int mod =1e9+7;
-int fact[1001];
+int mod = 1e9+7;
+vector<vector<int>>dp;
+    int solve(int ind,int stage,int &n,int &x,int &y){
+        if(ind==n )return stage<=x;
+        if(stage>x)return 0;
 
-int powm(int x, int n){
-    int result = 1;
-    x %= mod;
-    while(n > 0){
-        if(n & 1){
-            result = (result * x) % mod;
-        }
-        x = (x * x) % mod;
-        n >>= 1;
+        if(dp[ind][stage]!=-1)return dp[ind][stage];
+        int newStage = 1LL*(x-stage)*y*solve(ind+1,stage+1,n,x,y)%mod;
+        int oldStage = 1LL*(stage)*solve(ind+1,stage,n,x,y)%mod;
+
+        return dp[ind][stage]=(oldStage%mod + newStage%mod)%mod;
     }
-    return result;
-}
-
-    // Learn inverse mode
-    int mod_div(int p, int q) { return p % mod * powm(q, mod - 2) % mod; }
-
-    int nCr(int n,int r){
-        if(r>n)return 0;
-        return mod_div(fact[n],fact[n-r]*fact[r])%mod;
-    }
-
     int numberOfWays(int n, int x, int y) {
-        
-        // precompute factorinal
-        fact[0]=1;
-        for(int i =1;i<=1000;++i){
-            fact[i]= (fact[i-1]*i)%mod;
-        }
-
-        // calculate dp of n * x
-        vector<vector<int>>dp(n+1,vector<int>(x+1,0));
-        //base case
-        dp[0][0]=1;
-        dp[1][0]=0;
-        dp[0][1]=0;
-        for(int i=1;i<=n;++i){
-            for(int j =1;j<=x;++j){
-              dp[i][j] = ( (j * dp[i-1][j]) % mod + dp[i-1][j-1] ) % mod;
-            }
-        }
-
-        int ans =0;
-        for(int i =1;i<=x;++i){
-            ans=(ans + (nCr(x,i)*powm(y,i)%mod*dp[n][i]%mod*fact[i]%mod)%mod)%mod;
-        }
-        return ans;
-
+        dp.resize(n+1,vector<int>(x+1,-1));
+        return solve(0,0,n,x,y);
     }
 #undef int
 };
