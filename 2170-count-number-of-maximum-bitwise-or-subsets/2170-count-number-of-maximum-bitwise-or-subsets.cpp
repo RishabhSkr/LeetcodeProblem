435b17eb50ff81683192681a1d93f248a255309c
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-10-18 16:20:38 +0530

    Time: 31 ms (45.32%), Space: 24 MB (18.11%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -3,24 +3,25 @@ public:
         // generate subset meanwhile calculate the or check with maxXor val
         // if it is equal then we have found the or subset cnt++;
     int n;
-    void solve(int i,vector<int>&nums,int orVal,int &cnt,int &maxOR){
+    unordered_map<string ,int>mp; // memoize
+    int solve(int i,vector<int>&nums,int orVal,int &maxOR){
         if(i==n){
-            if(orVal==maxOR)cnt++;
-            return;
+            return (orVal==maxOR)? 1: 0;
         }
+        string key = to_string(i)+','+to_string(orVal);
+        if(mp.find(key)!=mp.end())return mp[key];
         // include it 
-        solve(i+1,nums,orVal|nums[i],cnt,maxOR);
+        int take = solve(i+1,nums,orVal|nums[i],maxOR);
         // not  include it
-        solve(i+1,nums,orVal,cnt,maxOR);
+        int notTake = solve(i+1,nums,orVal,maxOR);
+        return mp[key]=take+notTake;
 
         
     }
     int countMaxOrSubsets(vector<int>& nums) {
         n=nums.size();
         int k = 0;
-        int cnt=0;
         for(auto x : nums)k|=x;
-        solve(0,nums,0,cnt,k);
-        return cnt;
+        return solve(0,nums,0,k);
     }
 };
