2a244434729e08a0d22fcd3dde3edf199ec184ca
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-11-03 18:20:28 +0530

    Time: 52 ms (100%), Space: 38.3 MB (100%) - LeetHub

--- 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
@@ -6,13 +6,14 @@ public:
         m = moveTime[0].size();
 
         priority_queue<vector<int>,vector<vector<int>>, greater<vector<int>> >pq;
-        pq.push({0,0,0}); // minTime,i,j
 
         int dirn[][2] = {{-1,0},{0,-1},{1,0},{0,1}};
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        vis[0][0] = 1;
 
-        int ans =0;
+         // Track visited state including alterMove
+        vector<vector<vector<bool>>> vis(n, vector<vector<bool>>(m, vector<bool>(2, false)));
+
+        pq.push({0,0,0,0}); // minTime,i,j,alterMove
+        vis[0][0][0] = true;
 
         while(!pq.empty()){
             auto top = pq.top();
@@ -21,16 +22,24 @@ public:
             int curr = top[0];
             int i = top[1];
             int j = top[2];
+            int alterMove= top[3];
             if(i==n-1 && j==m-1)return curr;
             for(auto dir : dirn){
                 int newx = i+dir[0];
                 int newy = j+dir[1];
 
-                if(newx < 0 || newy < 0 || newx >= n || newy >= m || vis[newx][newy]) continue;
+                if(newx < 0 || newy < 0 || newx >= n || newy >= m || vis[newx][newy][alterMove]) continue;
+                int newtime = max(curr, moveTime[newx][newy]) +( (alterMove)? 2 : 1);
                 
-                int newtime = max(curr, moveTime[newx][newy]) + 1;
-                vis[newx][newy] = 1;
-                pq.push({newtime, newx, newy});
+                 // Next state will have opposite alterMove
+                int nextAlterMove = 1 - alterMove;
+                
+                // Skip if we've seen this state before
+                if(vis[newx][newy][nextAlterMove]) continue;
+
+                vis[newx][newy][nextAlterMove] = true;
+
+                pq.push({newtime, newx, newy,alterMove});
             }
         }
         return -1;
