ec7f7457d00bee5f0540c361caab82014497ff1c
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-11-04 19:54:39 +0530

    Time: 0 ms (100%), Space: 8.4 MB (59.19%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,53 +1,40 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& mat) {
-        int m = mat.size();
-        int n = mat[0].size();
-        int top = 0;
-        int down = m-1;
-        int left = 0;
-        int right = n-1;
         vector<int>ans;
-        // dirn = 0 ->left to right
-        //        1=top to down
-        //        2= right->left
-        //        3 = down to top
-        int dirn = 0;
+        int n = mat.size();
+        int m = mat[0].size();
+        int left  =0, top = 0, right = m-1,down = n-1;
         while(top<=down && left<=right){
-            if(dirn==0){
-                // const = row
-                // order = left to right 
-                for(int i =left;i<=right;++i){
+
+            //left to right side
+                for(int i = left ;i<= right ;++i){
                     ans.push_back(mat[top][i]);
                 }
                 top++;
-            }
-            if(dirn ==1){
-                // const = col
-                // order =  top to bottom 
-                for(int i =top;i<=down;++i){
+                // right to down side 
+                for(int i = top ;i<= down ;++i){
                     ans.push_back(mat[i][right]);
                 }
                 right--;
-            }
-            if(dirn==2){
-                // const = row
-                // order =  right to left 
-                for(int i =right;i>=left;--i){
-                    ans.push_back(mat[down][i]);
+
+                if(top<= down){
+                    // right to left
+                    for(int i = right ;i>= left ;--i){
+                        ans.push_back(mat[down][i]);
+                    }
+                    down--;
                 }
-                down--;
-            }
-            if(dirn == 3){
-                // const = col
-                // order =  down to top 
-                for(int i =down;i>=top;--i){
-                    ans.push_back(mat[i][left]);
+                
+                // left to top
+                if(left<=right){
+                    for(int i =  down;i>=top ;--i){
+                        ans.push_back(mat[i][left]);
+                    }
+                    left++;
                 }
-                left++;
-            }
-            dirn= (dirn+1)%4;
         }
         return ans;
+
     }
 };
