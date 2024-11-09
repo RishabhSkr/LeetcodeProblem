723b5cda361b102e0c4161e035a07bb38ff7087e
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2024-11-10 00:39:06 +0530

    Time: 1736 ms (11.61%), Space: 8.5 MB (52.8%) - LeetHub

--- 3394-minimum-array-end/3394-minimum-array-end.cpp
+++ 3394-minimum-array-end/3394-minimum-array-end.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     long long minEnd(int n, int x) {
-        long long start = x;
-        for(int i =2;i<=n;++i){
-            start=(start+1)|x;
+        long long num = x;
+        for(int i =1;i<n;++i){
+            num=(num+1)|x;
         }
-        return start;
+        return num;
      
     }
 };
