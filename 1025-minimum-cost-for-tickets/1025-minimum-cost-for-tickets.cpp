4ad124794c748a2885e05977d0737054d3b2c003
Rishabh Sonkar <68018650+RishabhSkr@users.noreply.github.com>
2025-01-01 00:28:55 +0530

    Time: 2 ms (31.67%), Space: 14.4 MB (12.28%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    int findNxtIdx(int i,int duration,vector<int>&days){
-        int curr = days[i]+duration-1;
-        int nxtIdx = i+1;
-        while(nxtIdx<days.size() && days[nxtIdx]<=curr )nxtIdx++;
-        return nxtIdx;
-    }
+    
     vector<int>dp;
-  int solve(int i, vector<int>& days, vector<int>& cost) {
-    if (i >= days.size()) return 0;
-    if(dp[i]!=-1)return dp[i];
-    int nxt1DayIdx = findNxtIdx(i, 1, days);
-    int cost1Day = cost[0] + solve(nxt1DayIdx, days, cost); 
+    unordered_map<int,int>mp;
+  int solve(int day, vector<int>& days, vector<int>& cost) {
+    if(day>365)return 0;
+    if(dp[day]!=-1)return dp[day];
+    if(mp.count(day)==0)return dp[day]=solve(day+1,days,cost);
+    int cost1Day = cost[0] + solve(day+1, days, cost); 
 
-    int nxt7DayIdx = findNxtIdx(i, 7, days);
-    int cost7Day = cost[1] + solve(nxt7DayIdx, days, cost);
+    int cost7Day = cost[1] + solve(day+7, days, cost);
 
-    int nxt30DayIdx = findNxtIdx(i, 30, days);
-    int cost30Day = cost[2] + solve(nxt30DayIdx, days, cost);
+    int cost30Day = cost[2] + solve(day+30, days, cost);
 
-    return dp[i]=min({cost1Day, cost7Day, cost30Day});
+    return dp[day]=min({cost1Day, cost7Day, cost30Day});
 }
 
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         // state curr idx store min cost store kro
-        dp.resize(days.size()+1,-1);
-        return solve(0, days, costs);
+       // Mark travel days in map
+        for(int day : days) {
+            mp[day] = 1;  // value doesn't matter, we just need to know if day exists
+        }
+        dp.resize(366,-1);
+        return solve(days[0], days, costs);
     }
 };
